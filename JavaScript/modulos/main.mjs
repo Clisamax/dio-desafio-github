41aefc4064c546965b126e3c84ea208b790159fd
clisamax <clisamax.gomes.dev@gmail.com>
2022-06-12 20:48:14 -0300

    parcel-bundler

---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./funções.mjs";
+
+console.log(mostraIdade('Clisamax', 34));
+console.log(mostraCidade('Clisamax', 'Limeira')); 
+console.log(mostraHobby('Clisamax', 'DEV'));
--- README.md
+++
@@ -1,5 +0,0 @@
-# repositório do desafio sobre Git/GitHub da DIO
-Desafio de projeto sobre Git/GitHub
-##Links úteis
-[sintaxe básica Markedown](https://www.markdownguide.org/basic-syntax/)
-
