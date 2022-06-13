6bd0de2435a6b8d674b975c2f05a2b25114b5e3d
clisamax <clisamax.gomes.dev@gmail.com>
2022-06-12 22:06:04 -0300

    at

--- JavaScript/modulos/main.mjs
+++
@@ -1,5 +0,0 @@
-import { mostraCidade, mostraIdade, mostraHobby} from "./funções.mjs";
-
-console.log(mostraIdade('Clisamax', 34));
-console.log(mostraCidade('Clisamax', 'Limeira')); 
-console.log(mostraHobby('Clisamax', 'DEV'));
